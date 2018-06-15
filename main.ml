466fedb3abbe9347fcf9134488b9195c28912e1e
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-06-15 10:11:00 +0900

    update

--- main.ml
+++ main.ml
@@ -3,13 +3,44 @@ open Syntax
 let position_to_string pos = 
   "file:" ^ pos.Lexing.pos_fname ^ ", line:" ^ string_of_int(pos.Lexing.pos_lnum) ^ ", col:" ^ string_of_int(pos.Lexing.pos_cnum)
 
+(* 環境。名前と term を紐付ける *)
+let empty_context : (string * term) list = []
+
+exception LambdaError of string
 
 let rec term_to_string term =
   match term with
   |TmVar(_, v) -> v
-  |TmAbs(_, v, t) -> "λ" ^ v ^ ". " ^ term_to_string t
-  |TmApp(_, t1, t2) -> (term_to_string t1) ^ " " ^ (term_to_string t2)
+  |TmAbs(_, v, t) -> "(λ" ^ v ^ ". " ^ term_to_string t ^ ")"
+  |TmApp(_, t1, t2) -> "(" ^ (term_to_string t1) ^ " " ^ (term_to_string t2) ^ ")"
+
+let rec apply ctx t1 t2 =
+  match t1 with
+  |TmAbs(_, v, t) ->
+      let ctx' = (v, t2)::ctx in
+      eval ctx' t
+  |_ -> raise (LambdaError "program error")
+  
+and eval ctx term =
+  match term with
+  |TmVar(_, v) ->
+      begin
+        try
+          let v' = List.assoc v ctx in
+          (ctx, v')
+        with Not_found -> (ctx, term)
+      end
+  |TmApp(_, t1, t2) ->
+      let _, t2' = eval ctx t2 in
+      let ctx', t1' = eval ctx t1 in
+      apply ctx' t1' t2'
+  |_ -> (ctx, term)
+
+
 
 let () =
-  let t = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+  let t = Parser.parse Lexer.main (Lexing.from_channel stdin) in
   print_endline (term_to_string t);
+  let _, r = eval empty_context t in 
+  print_endline (term_to_string r);
+
--- parser.mly
+++ parser.mly
@@ -5,18 +5,21 @@
 %token LPAREN RPAREN
 %token DOT LAMBDA
 %token EOL
-
 %token <string> STR
 
-%start toplevel
-%type <Syntax.term> toplevel
+%nonassoc STR LPAREN LAMBDA
+%left Apply
+
+%start parse
+%type <Syntax.term> parse
 
 %%
 
-toplevel: Term EOL { $1 }
+parse: term EOL { $1 }
 
-Term:
-  | LPAREN t=Term RPAREN { t }
-  | LAMBDA v=STR DOT t=Term { TmAbs($startpos, v, t) }
-  | t1=Term t2=Term { TmApp($startpos, t1, t2) }
-  | v=STR { TmVar($startpos, v) }
+term:
+  |LAMBDA v=STR DOT t=term { TmAbs($startpos, v, t) }
+  |STR { TmVar($startpos, $1) }
+  |LPAREN term RPAREN { $2 }
+  |term term { TmApp($startpos, $1, $2) } %prec Apply
+
